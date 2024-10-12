fb5269e3ba77cfd2336b8d09aefaf750d21bf0e3
InfinityScripter <talalaev.misha@gmail.com>
2024-10-12 17:40:56 +0300

    fix fetch calls

--- src/lib/services/callService.ts
+++ src/lib/services/callService.ts
@@ -1,17 +1,28 @@
 import axiosInstance from '@/lib/axiosInstance.ts';
 import {CallsApiResponse} from "@/types/callTypes.ts";
 
-export const fetchCalls = async (date_start: string, date_end: string, in_out: string = '', sort_by: string = 'date', order: string = 'DESC') => {
-    const response = await axiosInstance.post<CallsApiResponse>('/getList', {
-        date_start,
-        date_end,
-        in_out,
-        sort_by,
-        order,
-    });
+export const fetchCalls = async (
+    date_start: string,
+    date_end: string,
+    in_out: string = '',
+    sort_by: string = '',
+    order: string = ''
+) => {
+    const response = await axiosInstance.post<CallsApiResponse>(
+        '/getList',
+        null,
+        {
+            params: {
+                date_start,
+                date_end,
+                in_out,
+                sort_by,
+                order,
+            },
+        }
+    );
     return response.data;
 };
-
 
 export const fetchCallRecord = async (recordId: string, partnershipId: string): Promise<Blob> => {
     const response = await axiosInstance.post(
